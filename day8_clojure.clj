e3c86cc952a25fec59743d3c05d57c37ddf23b2c
Torkelv <torkel93@gmail.com>
2019-12-08 21:35:29 +0100

    refactor

--- day8_clojure.clj
+++ day8_clojure.clj
@@ -6,18 +6,17 @@
                    (apply min-key #(get % \0)))]
     (* (get layer \2) (get layer \1))))
 	
-(defn day8_part2 [width height]
-  (let [input (slurp "resources/day8input.txt")]
-    (->> input
-         seq
-         (partition (* width height))
-         (apply interleave)
-         (partition (quot (.length input) (* width height)))
-         (map (fn [v] (first (drop-while #(= \2 %) v))))
-         (partition width)
-         (map #(clojure.string/join "," %))
-         (clojure.string/join "\n")
-         )))
+(defn day8_part2 [width height pixels]
+  (->> pixels
+       seq
+       (partition (* width height))
+       (apply interleave)
+       (partition (quot (.length pixels) (* width height)))
+       (map (fn [v] (first (drop-while #(= \2 %) v))))
+       (partition width)
+       (map #(clojure.string/join "," %))
+       (clojure.string/join "\n")
+       ))
 	   
 (def day8-part1-answer (day8-part1 25 6 (slurp "resources/day8input.txt")))
 (def day8-part2-answer (day8-part2 25 6 (slurp "resources/day8input.txt")))
